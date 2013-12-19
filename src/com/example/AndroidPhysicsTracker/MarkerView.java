c5f5b5f351211b88f7b8176fe63b6eb65f2eff95
Clemens Zeidler <czei002@aucklanduni.ac.nz>
2013-12-19 13:06:19 +1300

    Implement a dragable marker.
    Once a marker is selected a drag handle is drawn. This enables to adjust
    the marker position precisely.

--- src/com/example/AndroidPhysicsTracker/MarkerView.java
+++ src/com/example/AndroidPhysicsTracker/MarkerView.java
@@ -1,27 +1,173 @@
 package com.example.AndroidPhysicsTracker;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+interface IMarker {
+    public void onDraw(Canvas canvas);
+
+    public boolean handleActionDown(MotionEvent ev);
+    public boolean handleActionUp(MotionEvent ev);
+    public boolean handleActionMove(MotionEvent ev);
+
+    public void setSelected(boolean selected);
+    public boolean isSelected();
+}
+
+
+/* DragableMarker can be selected. If it is selected there can also be a drag handler to move the marker.
+ */
+abstract class DragableMarker implements  IMarker {
+    protected PointF position;
+    protected PointF dragOffset;
+    protected boolean isSelected;
+    protected boolean isDragging;
+
+    public DragableMarker() {
+        position = new PointF(0, 0);
+        dragOffset = new PointF(0, 0);
+        isSelected = false;
+        isDragging = false;
+    }
+
+    public void setPosition(float x, float y) {
+        position.set(x, y);
+    }
+
+    public void setPosition(PointF pos) {
+        setPosition(pos.x, pos.y);
+    }
+
+    public PointF getPosition() {
+        return position;
+    }
+
+    public boolean handleActionDown(MotionEvent event) {
+        PointF point = new PointF(event.getX(), event.getY());
+        dragOffset.x = point.x - position.x;
+        dragOffset.y = point.y - position.y;
+
+        if (!isSelected) {
+            if (isPointOnSelectArea(point))
+                isSelected = true;
+            if (isPointOnDragArea(point))
+                isDragging = true;
+
+            if (isSelected || isDragging)
+                return true;
+        } else if (isPointOnDragArea(point)) {
+            isDragging = true;
+            return true;
+        }
+        isSelected = false;
+        isDragging = false;
+        return false;
+    }
+
+    public boolean handleActionUp(MotionEvent event) {
+        boolean wasDragging = isDragging;
+
+        isDragging = false;
+
+        return wasDragging;
+    }
+
+    public boolean handleActionMove(MotionEvent event) {
+        if (isDragging) {
+            PointF point = new PointF(event.getX(), event.getY());
+            point.x -= dragOffset.x;
+            point.y -= dragOffset.y;
+            onDraggedTo(point);
+            return true;
+        }
+        return false;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+        isDragging = false;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    protected void onDraggedTo(PointF point) {
+        setPosition(point);
+    }
+
+    abstract protected boolean isPointOnSelectArea(PointF point);
+
+    protected boolean isPointOnDragArea(PointF point) {
+        return isPointOnSelectArea(point);
+    }
+}
+
+
+class SimpleMarker extends DragableMarker {
+    final static float RADIUS = 30;
+    final static float RING_RADIUS = 100;
+    private Paint paint = null;
+
+    public SimpleMarker() {
+        paint = new Paint();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        float crossR = RADIUS / (float)1.41421356237;
+        paint.setColor(Color.argb(100, 20, 20, 20));
+        paint.setStrokeWidth(1);
+        canvas.drawLine(position.x - crossR, position.y - crossR, position.x + crossR, position.y + crossR, paint);
+        canvas.drawLine(position.x + crossR, position.y - crossR, position.x - crossR, position.y + crossR, paint);
+
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2);
+        canvas.drawCircle(position.x, position.y, RADIUS, paint);
+
+        if (isSelected()) {
+            paint.setColor(Color.argb(100, 0, 200, 100));
+            paint.setStrokeWidth(RADIUS);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(position.x, position.y, RING_RADIUS, paint);
+        }
+    }
+
+    @Override
+    protected boolean isPointOnSelectArea(PointF point) {
+        float distance = (float)Math.sqrt(Math.pow(point.x - position.x, 2) + Math.pow(point.y - position.y, 2));
+        return distance <= RADIUS;
+    }
+
+    protected boolean isPointOnDragArea(PointF point) {
+        float distance = (float)Math.sqrt(Math.pow(point.x - position.x, 2) + Math.pow(point.y - position.y, 2));
+        if (distance < RING_RADIUS + RADIUS / 2 && distance > RING_RADIUS - RADIUS / 2)
+            return true;
+        return isPointOnSelectArea(point);
+    }
+}
+
+
 public class MarkerView extends ViewGroup {
-    public MarkerView(Context context) {
+    private View targetView = null;
+
+    private IMarker selectedMarker = null;
+    private List<IMarker> markerList;
+    private Rect viewFrame = null;
+    private boolean touchEventHandledLastTime = false;
+
+    public MarkerView(Context context, View target) {
         super(context);
-        init();
-    }
-
-    public MarkerView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
-
-    public MarkerView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+        targetView = target;
         init();
     }
 
@@ -33,41 +179,75 @@ public class MarkerView extends ViewGroup {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-/*
-        int x = 10;
-        int y = 10;
-        int width = 700;
-        int height = 50;
 
-        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
-        drawable.getPaint().setColor(0xff74AC23);
-        drawable.setBounds(x, y, x + width, y + height);
-
-        drawable.draw(canvas);
-        */
-        setBackgroundColor(Color.TRANSPARENT);
-
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-
-        Rect frame = new Rect();
-        getDrawingRect(frame);
-        canvas.drawLine(frame.left, frame.top, frame.right, frame.bottom, paint);
-        canvas.drawLine(frame.right, frame.top, frame.left, frame.bottom, paint);
-
-        canvas.drawLine(400, frame.top, 400, frame.bottom, paint);
-
+        for (IMarker marker : markerList)
+            marker.onDraw(canvas);
     }
 
     @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (ev.getX() < 400)
-            return true;
-        return false;
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+        boolean handled = false;
+        if (action == MotionEvent.ACTION_DOWN) {
+            for (IMarker marker : markerList) {
+                if (marker.handleActionDown(event)) {
+                    handled = true;
+                    if (marker.isSelected())
+                        selectedMarker = marker;
+                    break;
+                }
+            }
+        } else if (action == MotionEvent.ACTION_UP) {
+            for (IMarker marker : markerList) {
+                if (marker.handleActionUp(event)) {
+                    handled = true;
+                    break;
+                }
+            }
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            if (viewFrame.contains((int)event.getX(), (int)event.getY())) {
+                for (IMarker marker : markerList) {
+                    if (marker.handleActionMove(event)) {
+                        handled = true;
+                        break;
+                    }
+                }
+            } else
+                handled = touchEventHandledLastTime;
+        }
+        if (handled)
+            invalidate();
+
+        if (selectedMarker != null && !handled) {
+            selectedMarker.setSelected(false);
+            selectedMarker = null;
+            invalidate();
+        }
+        touchEventHandledLastTime = handled;
+        return handled;
+    }
+
+    @Override
+    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec) {
+        Rect rect = new Rect();
+        targetView.getDrawingRect(rect);
+        setMeasuredDimension(rect.width(), rect.height());
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        viewFrame.right = w;
+        viewFrame.bottom = h;
     }
 
     private void init() {
         setWillNotDraw(false);
+
+        viewFrame = new Rect();
+        getDrawingRect(viewFrame);
+
+        markerList = new ArrayList<IMarker>();
+        markerList.add(new SimpleMarker());
     }
 
 }
