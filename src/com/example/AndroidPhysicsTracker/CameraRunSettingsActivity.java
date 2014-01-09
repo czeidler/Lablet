6f2c3f906423546a9d9e82d5c44850cdf6801dac
Clemens <czei002@aucklanduni.ac.nz>
2014-01-09 16:53:23 +1300

    Camera run settings GUI is working.
    However, the values are not stored in the experiment yet.

--- src/com/example/AndroidPhysicsTracker/CameraRunSettingsActivity.java
+++ src/com/example/AndroidPhysicsTracker/CameraRunSettingsActivity.java
@@ -1,14 +1,22 @@
 package com.example.AndroidPhysicsTracker;
 
 import android.os.Bundle;
-import android.widget.SeekBar;
+import android.view.View;
+import android.widget.*;
 
 import java.io.File;
 
 
 public class CameraRunSettingsActivity extends ExperimentActivity {
-    CameraExperiment cameraExperiment;
-    VideoFrameView videoFrameView;
+    private CameraExperiment cameraExperiment;
+    private VideoFrameView videoFrameView;
+
+    private SeekBar seekBar = null;
+    private EditText editVideoStart = null;
+    private EditText editVideoEnd = null;
+
+    private int videoStartValue;
+    private int videoEndValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,11 +29,11 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
         setContentView(R.layout.camera_run_settings);
 
         videoFrameView = (VideoFrameView)findViewById(R.id.videoFrameView);
-        File storageDir = experiment.getStorageDir();
+        File storageDir = cameraExperiment.getStorageDir();
         File videoFile = new File(storageDir, cameraExperiment.getVideoFileName());
         videoFrameView.setVideoFilePath(videoFile.getPath());
 
-        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
         int duration = cameraExperiment.getVideoDuration();
         seekBar.setMax(duration);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -46,6 +54,53 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
 
             }
         });
+
+        NumberPicker frameRatePicker = (NumberPicker)findViewById(R.id.frameRatePicker);
+        frameRatePicker.setMinValue(1);
+        frameRatePicker.setMaxValue(videoFrameView.getVideoFrameRate());
+        frameRatePicker.setValue(cameraExperiment.getAnalysisFrameRate());
+
+        videoStartValue = cameraExperiment.getAnalysisVideoStart();
+        videoEndValue = cameraExperiment.getAnalysisVideoEnd();
+
+        editVideoStart = (EditText)findViewById(R.id.editStart);
+        String string = "";
+        string += videoStartValue;
+        editVideoStart.setText(string);
+        editVideoEnd = (EditText)findViewById(R.id.editEnd);
+        string = "";
+        string += videoEndValue;
+        editVideoEnd.setText(string);
+
+        Button setStartButton = (Button)findViewById(R.id.buttonSetStart);
+        setStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (seekBar.getProgress() >= videoEndValue) {
+                    toastMessage("Start value must be smaller than the end value!");
+                    return;
+                }
+                videoStartValue = seekBar.getProgress();
+                String string = "";
+                string += videoStartValue;
+                editVideoStart.setText(string);
+            }
+        });
+
+        Button setEndButton = (Button)findViewById(R.id.buttonSetEnd);
+        setEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (videoStartValue >= seekBar.getProgress()) {
+                    toastMessage("Start value must be smaller than the end value!");
+                    return;
+                }
+                videoEndValue = seekBar.getProgress();
+                String string = "";
+                string += videoEndValue;
+                editVideoEnd.setText(string);
+            }
+        });
     }
 
     @Override
@@ -55,5 +110,10 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
 
     public void seekTo(int time) {
         videoFrameView.seekToFrame(time * 1000);
+    }
+
+    protected void toastMessage(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
