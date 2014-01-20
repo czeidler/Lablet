ae49af98e752f457e3f6befabc59b83f2a42a84e
Clemens <czei002@aucklanduni.ac.nz>
2014-01-21 08:25:39 +1300

    Improve video settings activity.
    - Only allow valid frame rates.
    - Add label for the frame length.

--- src/com/example/AndroidPhysicsTracker/CameraRunSettingsActivity.java
+++ src/com/example/AndroidPhysicsTracker/CameraRunSettingsActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.*;
 
 import java.io.File;
+import java.util.ArrayList;
 
 
 public class CameraRunSettingsActivity extends ExperimentActivity {
@@ -18,9 +19,12 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
     private NumberPicker frameRatePicker = null;
     private EditText editVideoStart = null;
     private EditText editVideoEnd = null;
+    private EditText editFrameLength = null;
 
     private int videoStartValue;
     private int videoEndValue;
+
+    private ArrayList<Integer> frameRateList;
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
@@ -50,6 +54,8 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        frameRateList = new ArrayList<Integer>();
+
         Intent intent = getIntent();
         if (!loadExperiment(getIntent()))
             return;
@@ -62,6 +68,7 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
         File storageDir = cameraExperiment.getStorageDir();
         File videoFile = new File(storageDir, cameraExperiment.getVideoFileName());
         videoFrameView.setVideoFilePath(videoFile.getPath());
+        videoFrameView.seekToFrame(0);
 
         seekBar = (SeekBar)findViewById(R.id.seekBar);
         int duration = cameraExperiment.getVideoDuration();
@@ -85,9 +92,15 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
             }
         });
 
+        editFrameLength = (EditText)findViewById(R.id.editFrameLength);
+
         frameRatePicker = (NumberPicker)findViewById(R.id.frameRatePicker);
-        frameRatePicker.setMinValue(1);
-        frameRatePicker.setMaxValue(videoFrameView.getVideoFrameRate());
+        frameRatePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int old, int newValue) {
+                setFrameRateLengthEdit(frameRateList.get(newValue));
+            }
+        });
 
         editVideoStart = (EditText)findViewById(R.id.editStart);
         editVideoEnd = (EditText)findViewById(R.id.editEnd);
@@ -102,12 +115,18 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
             cameraExperiment.setAnalysisVideoStart(runSettings.getInt("analysis_video_start"));
             cameraExperiment.setAnalysisVideoEnd(runSettings.getInt("analysis_video_end"));
         }
-        int frameRate = cameraExperiment.getAnalysisFrameRate();
+        int analysisFrameRate = cameraExperiment.getAnalysisFrameRate();
         videoStartValue = cameraExperiment.getAnalysisVideoStart();
         videoEndValue = cameraExperiment.getAnalysisVideoEnd();
 
         // initial views with values
-        frameRatePicker.setValue(frameRate);
+        calculateFrameRateValues(videoFrameView.getVideoFrameRate());
+        frameRatePicker.setMinValue(0);
+        frameRatePicker.setMaxValue(frameRateList.size() - 1);
+        frameRatePicker.setDisplayedValues(getFrameRateStringList());
+        int pickerFrameRateIndex = getNumberPickerIndexForFrameRate(analysisFrameRate);
+        frameRatePicker.setValue(pickerFrameRateIndex);
+        setFrameRateLengthEdit(frameRateList.get(pickerFrameRateIndex));
         setVideoStart(videoStartValue);
         setVideoEnd(videoEndValue);
 
@@ -136,7 +155,41 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
                 setVideoEnd(videoEndValue);
             }
         });
+    }
 
+    private void calculateFrameRateValues(int maxFrameRate) {
+        frameRateList.clear();
+        frameRateList.add(1);
+        for (int i = 2; i < maxFrameRate; i++) {
+            if (maxFrameRate % i == 0)
+                frameRateList.add(i);
+        }
+        frameRateList.add(maxFrameRate);
+    }
+
+    private String[] getFrameRateStringList() {
+        String[] list = new String[frameRateList.size()];
+        for (int i = 0; i < frameRateList.size(); i++) {
+            String stringValue = "";
+            stringValue += frameRateList.get(i);
+            list[i] = stringValue;
+        }
+        return list;
+    }
+
+    private int getNumberPickerIndexForFrameRate(int frameRate) {
+        for (int i = 0; i < frameRateList.size(); i++) {
+            if (frameRateList.get(i) == frameRate)
+                return i;
+        }
+        return frameRateList.size() - 1;
+    }
+
+    private void setFrameRateLengthEdit(int frameRate) {
+        float length = 1.0f / frameRate * 1000;
+        String stringValue = "";
+        stringValue += (int)length;
+        editFrameLength.setText(stringValue);
     }
 
     private void setVideoStart(int value) {
@@ -154,7 +207,7 @@ public class CameraRunSettingsActivity extends ExperimentActivity {
     private void applySettingsAndFinish() {
         Intent intent = new Intent();
 
-        intent.putExtra("analysis_frame_rate", frameRatePicker.getValue());
+        intent.putExtra("analysis_frame_rate", frameRateList.get(frameRatePicker.getValue()));
         intent.putExtra("analysis_video_start", Integer.parseInt(editVideoStart.getText().toString()));
         intent.putExtra("analysis_video_end", Integer.parseInt(editVideoEnd.getText().toString()));
 
