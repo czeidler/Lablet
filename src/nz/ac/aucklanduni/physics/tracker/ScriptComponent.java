d3d6953a3747fb90d334bf0d3f7f590c47eb98b1
Clemens <czei002@aucklanduni.ac.nz>
2014-02-24 14:03:56 +1300

    Add ability to save and load a script.

--- src/nz/ac/aucklanduni/physics/tracker/ScriptComponent.java
+++ src/nz/ac/aucklanduni/physics/tracker/ScriptComponent.java
@@ -10,9 +10,39 @@ package nz.ac.aucklanduni.physics.tracker;
 
 import android.os.Bundle;
 
+import java.security.MessageDigest;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+
+class Hash {
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        // from: http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static String sha1Hex(String data) {
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(data.getBytes("UTF-8"));
+            sha1 = bytesToHex(crypt.digest());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+}
 
 public class ScriptComponent {
     final static public int SCRIPT_STATE_INACTIVE = -2;
@@ -24,6 +54,66 @@ public class ScriptComponent {
     private int state = SCRIPT_STATE_INACTIVE;
     private Map<Integer, ScriptComponent> connections = new HashMap<Integer, ScriptComponent>();
 
+    static class Iterator {
+        private ScriptComponent currentComponent;
+        private java.util.Iterator<ScriptComponent> currentComponentIterator;
+        private ScriptComponent.Iterator childIterator;
+
+        Iterator(ScriptComponent root) {
+            currentComponent = root;
+            currentComponentIterator = currentComponent.connections.values().iterator();
+        }
+
+        ScriptComponent next() {
+            ScriptComponent child = currentComponentIterator.next();
+            if (child == null)
+                return null;
+
+            if (childIterator == null) {
+                childIterator = new Iterator(child);
+                return child;
+            } else {
+                child = childIterator.next();
+                if (child == null) {
+                    childIterator = null;
+                    return next();
+                } else
+                    return child;
+            }
+        }
+    }
+
+    static String getChainHash(ScriptComponent component) {
+        String hashData = component.getName();
+        java.util.Iterator<ScriptComponent> iterator = component.connections.values().iterator();
+        int childId = -1;
+        while (true) {
+            ScriptComponent child = iterator.next();
+            if (child == null)
+                break;
+            childId++;
+
+            hashData += childId;
+            hashData += getChainHash(child);
+        }
+
+        return Hash.sha1Hex(hashData);
+    }
+
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    public void toBundle(Bundle bundle) {
+        bundle.putInt("state", state);
+    }
+
+    public boolean fromBundle(Bundle bundle) {
+        if (!bundle.containsKey("state"))
+            return false;
+        state = bundle.getInt("state");
+        return true;
+    }
 
     public void setNextComponent(int state, ScriptComponent component) {
         connections.put(state, component);
@@ -71,7 +161,7 @@ public class ScriptComponent {
         ScriptComponent currentParent = parent;
         while (currentParent != null) {
             stepsToRoot++;
-            currentParent = parent.getParent();
+            currentParent = currentParent.getParent();
         }
         return stepsToRoot;
     }
@@ -111,6 +201,49 @@ class Script {
             return false;
 
         setCurrentComponent(root);
+        return true;
+    }
+
+    public void saveScript(Bundle bundle) {
+        bundle.putString("scriptId", ScriptComponent.getChainHash(root));
+
+        int componentId = -1;
+        ScriptComponent.Iterator iterator = new ScriptComponent.Iterator(root);
+        while (true) {
+            ScriptComponent component = iterator.next();
+            if (component == null)
+                break;
+            componentId++;
+
+            Bundle componentBundle = new Bundle();
+            component.toBundle(componentBundle);
+
+            String bundleKey = Integer.toString(componentId);
+            bundle.putBundle(bundleKey, componentBundle);
+        }
+    }
+
+    public boolean loadScript(Bundle bundle) {
+        String scriptId = ScriptComponent.getChainHash(root);
+        if (!bundle.get("scriptId").equals(scriptId)) {
+            return false;
+        }
+
+        int componentId = -1;
+        ScriptComponent.Iterator iterator = new ScriptComponent.Iterator(root);
+        while (true) {
+            ScriptComponent component = iterator.next();
+            if (component == null)
+                break;
+            componentId++;
+
+            String bundleKey = Integer.toString(componentId);
+            if (!bundle.containsKey(bundleKey))
+                return false;
+            if (!component.fromBundle(bundle.getBundle(bundleKey)))
+                return false;
+        }
+
         return true;
     }
 
