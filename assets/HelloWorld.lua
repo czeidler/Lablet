0404d23419d32a6a6af71140219dc53f2bcf2b9a
Clemens <czei002@aucklanduni.ac.nz>
2014-03-03 19:02:55 +1300

    Update script file.

--- assets/HelloWorld.lua
+++ assets/HelloWorld.lua
@@ -1,9 +1,10 @@
 
 function onBuildExperimentScript(scriptBuilder)
-	local questions1 = scriptBuilder:create("QuestionsComponent")
+	local questions1 = scriptBuilder:create("Sheet")
 	questions1:setTitle("Q1:")
-	questions1:addTextOnlyQuestion("Text only Question")
-	questions1:addTextOnlyQuestion("Text only Question2")
+	questions1:addText("Text only Question")
+	questions1:addText("Text only Question2")
+	questions1:addCheckQuestion("check")
 	scriptBuilder:add(questions1)
 
 	local cameraExperiment = scriptBuilder:create("CameraExperiment")
@@ -17,11 +18,19 @@ function onBuildExperimentScript(scriptBuilder)
 	experimentAnalysis:setDescriptionText("Please analyze the free fall video:")
 	scriptBuilder:add(experimentAnalysis)
 	
+	local experimentSheet = scriptBuilder:create("ExperimentSheet")
+	experimentSheet:setExperiment(experiment)
+	experimentSheet:setLayoutType("horizontal")
+	experimentSheet:setTitle("Experiment Sheet:")
+	experimentSheet:addPositionGraph()
+	experimentSheet:addPositionGraph()
+	scriptBuilder:add(experimentSheet)
+	
 	local calculateYSpeed = scriptBuilder:create("CalculateYSpeed")
 	calculateYSpeed:setExperiment(experiment)
 	scriptBuilder:add(calculateYSpeed)
 
-	local questions2 = scriptBuilder:create("QuestionsComponent")
+	local questions2 = scriptBuilder:create("Sheet")
 	questions2:setTitle("Q2:")
 	scriptBuilder:add(questions2)
 	
