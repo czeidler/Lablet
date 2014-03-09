77ad480dd19e3f40d078571c09063d15263ba181
Clemens <czei002@aucklanduni.ac.nz>
2014-03-10 12:47:49 +1300

    Add ScriptComponentQuestion, refactoring and visual improvements.
    - Add a general purpose counter to the sheet component.
    - Add a question component that uses such a counter to display text,
    e.g. "Q4: ...".
    - Move the core part of the ScripComponentTreeSheet into a separate
    class.
    - Set background color of the check box and the question component.

--- src/nz/ac/aucklanduni/physics/tracker/script/ScriptComponentTreeSheet.java
+++ src/nz/ac/aucklanduni/physics/tracker/script/ScriptComponentTreeSheet.java
@@ -10,11 +10,17 @@ package nz.ac.aucklanduni.physics.tracker.script;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import nz.ac.aucklanduni.physics.tracker.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static nz.ac.aucklanduni.physics.tracker.R.*;
 
 
 class TextComponent extends ScriptComponentViewHolder {
@@ -49,6 +55,7 @@ class CheckBoxQuestion extends ScriptComponentViewHolder {
     public View createView(Context context, android.support.v4.app.Fragment parent) {
         CheckBox view = new CheckBox(context);
         view.setTextAppearance(context, android.R.style.TextAppearance_Medium);
+        view.setBackgroundColor(context.getResources().getColor(color.sc_question_background_color));
         view.setText(text);
 
         if (getState() == ScriptComponentTree.SCRIPT_STATE_DONE)
@@ -64,6 +71,35 @@ class CheckBoxQuestion extends ScriptComponentViewHolder {
             }
         });
         return view;
+    }
+
+    @Override
+    public boolean initCheck() {
+        return true;
+    }
+}
+
+class ScriptComponentQuestion extends ScriptComponentViewHolder {
+    private String text = "";
+    private ScriptComponentTreeSheetBase component;
+    private ScriptComponentTreeSheetBase.Counter counter;
+
+    public ScriptComponentQuestion(String text, ScriptComponentTreeSheetBase component) {
+        this.text = text;
+        this.component = component;
+        counter = this.component.getCounter("QuestionCounter");
+
+        setState(ScriptComponentTree.SCRIPT_STATE_DONE);
+    }
+
+    @Override
+    public View createView(Context context, android.support.v4.app.Fragment parent) {
+        TextView textView = new TextView(context);
+        textView.setTextAppearance(context, android.R.style.TextAppearance_Medium);
+        textView.setBackgroundColor(context.getResources().getColor(color.sc_question_background_color));
+
+        textView.setText("Q" + counter.increaseValue() + ": " + text);
+        return textView;
     }
 
     @Override
@@ -91,12 +127,26 @@ abstract class SheetLayout {
 }
 
 
-public class ScriptComponentTreeSheet extends ScriptComponentTreeFragmentHolder {
+class ScriptComponentTreeSheetBase extends ScriptComponentTreeFragmentHolder {
+    public class Counter {
+        private int counter = 0;
+
+        public void setValue(int value) {
+            counter = value;
+        }
+
+        public int increaseValue() {
+            counter++;
+            return counter;
+        }
+    }
+
     private SheetGroupLayout sheetGroupLayout = new SheetGroupLayout(true);
     private ScriptComponentContainer<ScriptComponentViewHolder> itemContainer
             = new ScriptComponentContainer<ScriptComponentViewHolder>();
+    private Map<String, Counter> mapOfCounter = new HashMap<String, Counter>();
 
-    public ScriptComponentTreeSheet(Script script) {
+    public ScriptComponentTreeSheetBase(Script script) {
         super(script);
 
         itemContainer.setListener(new ScriptComponentContainer.IItemContainerListener() {
@@ -147,10 +197,63 @@ public class ScriptComponentTreeSheet extends ScriptComponentTreeFragmentHolder 
             sheetGroupLayout.setOrientation(true);
     }
 
+    public SheetGroupLayout addGroupLayout(boolean vertical, SheetGroupLayout parent) {
+        SheetGroupLayout layout = new SheetGroupLayout(vertical);
+        if (parent == null)
+            sheetGroupLayout.addLayout(layout);
+        else
+            parent.addLayout(layout);
+        return layout;
+    }
+
+    protected SheetLayout addItemViewHolder(ScriptComponentViewHolder item,
+                                            SheetGroupLayout parent) {
+        itemContainer.addItem(item);
+        SheetLayout layoutItem;
+        if (parent == null)
+            layoutItem = sheetGroupLayout.addView(item);
+        else
+            layoutItem = parent.addView(item);
+        return layoutItem;
+    }
+
+    /**
+     * The counter is valid for the lifetime of the fragment view. If not counter with the given name exist, a new
+     * counter is created.
+     * @param name counter name
+     * @return a Counter
+     */
+    public Counter getCounter(String name) {
+        if (!mapOfCounter.containsKey(name)) {
+            Counter counter = new Counter();
+            mapOfCounter.put(name, counter);
+            return counter;
+        }
+        return mapOfCounter.get(name);
+    }
+
+    public void resetCounter() {
+        mapOfCounter.clear();
+    }
+}
+
+
+public class ScriptComponentTreeSheet extends ScriptComponentTreeSheetBase {
+
+    public ScriptComponentTreeSheet(Script script) {
+        super(script);
+    }
+
     public ScriptComponentViewHolder addText(String text, SheetGroupLayout parent) {
         TextComponent textOnlyQuestion = new TextComponent(text);
         addItemViewHolder(textOnlyQuestion, parent);
         return textOnlyQuestion;
+    }
+
+    public ScriptComponentViewHolder addQuestion(String text, SheetGroupLayout parent) {
+        ScriptComponentQuestion component = new ScriptComponentQuestion(text, this);
+        addItemViewHolder(component, parent);
+        return component;
     }
 
     public ScriptComponentViewHolder addCheckQuestion(String text, SheetGroupLayout parent) {
@@ -169,26 +272,6 @@ public class ScriptComponentTreeSheet extends ScriptComponentTreeFragmentHolder 
         ScriptComponentPotentialEnergy1 question = new ScriptComponentPotentialEnergy1();
         addItemViewHolder(question, parent);
         return question;
-    }
-
-    public SheetGroupLayout addGroupLayout(boolean vertical, SheetGroupLayout parent) {
-        SheetGroupLayout layout = new SheetGroupLayout(vertical);
-        if (parent == null)
-            sheetGroupLayout.addLayout(layout);
-        else
-            parent.addLayout(layout);
-        return layout;
-    }
-
-    protected SheetLayout addItemViewHolder(ScriptComponentViewHolder item,
-                                                                 SheetGroupLayout parent) {
-        itemContainer.addItem(item);
-        SheetLayout layoutItem;
-        if (parent == null)
-            layoutItem = sheetGroupLayout.addView(item);
-        else
-            layoutItem = parent.addView(item);
-        return layoutItem;
     }
 }
 
@@ -215,7 +298,7 @@ class ScriptComponentSheetFragment extends ScriptComponentGenericFragment {
     private FrameLayout sheetLayout = null;
     private int childViewThatHasStartedAnActivity = -1;
 
-    public ScriptComponentSheetFragment(ScriptComponentTreeSheet component) {
+    public ScriptComponentSheetFragment(ScriptComponentTreeSheetBase component) {
         super(component);
     }
 
@@ -224,13 +307,14 @@ class ScriptComponentSheetFragment extends ScriptComponentGenericFragment {
                              Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
 
-        View child = setChild(R.layout.script_component_sheet_fragment);
+        View child = setChild(layout.script_component_sheet_fragment);
         assert child != null;
 
-        sheetLayout = (FrameLayout)child.findViewById(R.id.sheetLayout);
+        sheetLayout = (FrameLayout)child.findViewById(id.sheetLayout);
         assert sheetLayout != null;
 
         ScriptComponentTreeSheet sheetComponent = (ScriptComponentTreeSheet)component;
+        sheetComponent.resetCounter();
         View sheetView = sheetComponent.getSheetLayout().buildLayout(getActivity(), this);
         sheetLayout.addView(sheetView);
 
