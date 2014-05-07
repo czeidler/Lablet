2705442e6f21e8c0e30ae43f92d62821994b80ac
Clemens <czei002@aucklanduni.ac.nz>
2014-05-08 09:17:51 +1200

    Add a simple storage class for various file operations.

---
+++ src/nz/ac/aucklanduni/physics/tracker/StorageLib.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package nz.ac.aucklanduni.physics.tracker;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * Collection of storage related operations.
+ *
+ * Note: these function may take a significant amount of time depending where the files are located. For example,
+ * moving a file on the same storage is faster than moving it somewhere to a different storage because this would
+ * involve a copy and a delete operation.
+ *
+ * TODO: add listener interfaces to monitor copy, move, delete... progress
+ */
+public class StorageLib {
+
+    /**
+     * I file is a directory it deletes it recursively. If it is just a file it just deletes this file.
+     * @param file
+     * @return false on the first file that can't be deleted
+     */
+    static public boolean recursiveDeleteFile(File file) {
+        if (file.isDirectory()) {
+            String[] children = file.list();
+            for (String child : children) {
+                if (!recursiveDeleteFile(new File(file, child)))
+                    return false;
+            }
+        }
+        return file.delete();
+    }
+
+    static public boolean moveFile(File source, File destination) {
+        if (source.isDirectory())
+            return false;
+        // first try to just rename the file
+        if (source.renameTo(destination))
+            return true;
+        // this could have failed because the file is on different storage cards so to a hard copy and then delete it
+        if (!copyFile(source, destination))
+            return false;
+
+        return source.delete();
+    }
+
+    /**
+     * Copies a file (not a directory) from source to destination
+     * @param source
+     * @param destination
+     * @return
+     */
+    static public boolean copyFile(File source, File destination) {
+        try {
+            FileInputStream inStream = new FileInputStream(source);
+            FileOutputStream outStream = new FileOutputStream(destination);
+            FileChannel inChannel = inStream.getChannel();
+            inChannel.transferTo(0, inChannel.size(), outStream.getChannel());
+            inStream.close();
+            outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
