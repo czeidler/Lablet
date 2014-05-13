f8eee9d1397ebbf465894d7cbcefdc1a5c8e30f8
Clemens <czei002@aucklanduni.ac.nz>
2014-05-13 14:43:24 +1200

    Make selectable what is shown in the graph.

--- src/nz/ac/aucklanduni/physics/tracker/AnalysisMixedDataFragment.java
+++ src/nz/ac/aucklanduni/physics/tracker/AnalysisMixedDataFragment.java
@@ -7,22 +7,47 @@
  */
 package nz.ac.aucklanduni.physics.tracker;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import nz.ac.aucklanduni.physics.tracker.*;
 import nz.ac.aucklanduni.physics.tracker.views.RunContainerView;
-import nz.ac.aucklanduni.physics.tracker.views.graph.GraphView2D;
-import nz.ac.aucklanduni.physics.tracker.views.graph.MarkerGraphAdapter;
-import nz.ac.aucklanduni.physics.tracker.views.graph.XPositionMarkerGraphAxis;
-import nz.ac.aucklanduni.physics.tracker.views.graph.YPositionMarkerGraphAxis;
+import nz.ac.aucklanduni.physics.tracker.views.graph.*;
 import nz.ac.aucklanduni.physics.tracker.views.table.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnalysisMixedDataFragment extends android.support.v4.app.Fragment {
     private RunContainerView runContainerView = null;
     private TableView tableView = null;
     private GraphView2D graphView = null;
+    private Spinner graphSpinner = null;
+    private List<GraphSpinnerEntry> graphSpinnerEntryList = new ArrayList<GraphSpinnerEntry>();
+
+    private class GraphSpinnerEntry {
+        private String name;
+        private MarkerGraphAdapter markerGraphAdapter;
+
+        public GraphSpinnerEntry(String name, MarkerGraphAdapter adapter) {
+            this.name = name;
+            this.markerGraphAdapter = adapter;
+        }
+
+        public String toString() {
+            return name;
+        }
+
+        public MarkerGraphAdapter getMarkerGraphAdapter() {
+            return markerGraphAdapter;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,8 +85,32 @@ public class AnalysisMixedDataFragment extends android.support.v4.app.Fragment {
 
         // marker graph view
         graphView = (GraphView2D)view.findViewById(R.id.tagMarkerGraphView);
-        graphView.setAdapter(new MarkerGraphAdapter(experimentAnalysis, "Position Data", new XPositionMarkerGraphAxis(),
-                new YPositionMarkerGraphAxis()));
+
+        // graph spinner
+        graphSpinnerEntryList.add(new GraphSpinnerEntry("Position Data", new MarkerGraphAdapter(experimentAnalysis,
+                "Position Data", new XPositionMarkerGraphAxis(), new YPositionMarkerGraphAxis())));
+        graphSpinnerEntryList.add(new GraphSpinnerEntry("x-Velocity", new MarkerGraphAdapter(experimentAnalysis,
+                "x-Velocity", new TimeMarkerGraphAxis(), new XSpeedMarkerGraphAxis())));
+        graphSpinnerEntryList.add(new GraphSpinnerEntry("y-Velocity", new MarkerGraphAdapter(experimentAnalysis,
+                "y-Velocity", new TimeMarkerGraphAxis(), new YSpeedMarkerGraphAxis())));
+
+        graphSpinner = (Spinner)view.findViewById(R.id.graphSpinner);
+        graphSpinner.setAdapter(new ArrayAdapter<GraphSpinnerEntry>(getActivity(), android.R.layout.simple_spinner_item,
+                graphSpinnerEntryList));
+        graphSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                GraphSpinnerEntry entry = graphSpinnerEntryList.get(i);
+                graphView.setAdapter(entry.getMarkerGraphAdapter());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                graphView.setAdapter(null);
+            }
+        });
+        graphSpinner.setSelection(0);
+
         return view;
     }
 
