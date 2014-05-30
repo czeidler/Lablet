b4961ab31b1984cc24b858a587f3773e1ff4c42b
Clemens <czei002@aucklanduni.ac.nz>
2014-05-30 14:38:18 +1200

    Add a pressed state for the media buttons.

--- src/nz/ac/auckland/lablet/experiment/Experiment.java
+++ src/nz/ac/auckland/lablet/experiment/Experiment.java
@@ -50,7 +50,7 @@ abstract public class Experiment {
     /**
      * The raw data is stored in internal units starting at (0,0). These methods return the max values.
      * The max values ratio should be the same as the screen ratio.
-     * 
+     *
      * @return the max raw value
      */
     abstract public float getMaxRawX();
