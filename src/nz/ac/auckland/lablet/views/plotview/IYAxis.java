ee27e1b46797e3071c1adce51b5389c57414935e
Clemens <czei002@aucklanduni.ac.nz>
2014-07-04 15:18:07 +1200

    Add licence.

--- src/nz/ac/auckland/lablet/views/plotview/IYAxis.java
+++ src/nz/ac/auckland/lablet/views/plotview/IYAxis.java
@@ -1,4 +1,12 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
 package nz.ac.auckland.lablet.views.plotview;
+
 
 public interface IYAxis {
     /**
--- src/nz/ac/auckland/lablet/views/plotview/PlotView.java
+++ src/nz/ac/auckland/lablet/views/plotview/PlotView.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
 package nz.ac.auckland.lablet.views.plotview;
 
 import android.content.Context;
