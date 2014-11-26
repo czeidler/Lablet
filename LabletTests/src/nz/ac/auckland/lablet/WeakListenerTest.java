c6f5f0431e281d5187a0fdf07b63902f805cc02d
Clemens <czei002@aucklanduni.ac.nz>
2014-11-26 16:08:13 +1300

    Add WeakListenerTest.

---
+++ LabletTests/src/nz/ac/auckland/lablet/WeakListenerTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package nz.ac.auckland.lablet;
+
+import junit.framework.TestCase;
+import nz.ac.auckland.lablet.misc.WeakListenable;
+
+import java.lang.ref.WeakReference;
+
+
+public class WeakListenerTest extends TestCase {
+    interface IListener {
+        void onEvent();
+    }
+
+    class DataClass extends WeakListenable<IListener> {
+
+    }
+
+    class ListenerClass {
+        IListener listener = new IListener() {
+            @Override
+            public void onEvent() {
+
+            }
+        };
+
+        public ListenerClass(DataClass data) {
+            data.addListener(listener);
+        }
+    }
+
+    public void test() {
+        DataClass data = new DataClass();
+
+        ListenerClass listener = new ListenerClass(data);
+        ListenerClass listener2 = new ListenerClass(data);
+
+        assertEquals(2, data.getListeners().size());
+
+        // check if de-referring a listener makes it disappear from the listener list
+        listener = null;
+        System.gc();
+        assertEquals(1, data.getListeners().size());
+
+        // check if the remaining listener prevents the data class from being deleted
+        WeakReference<DataClass> weakReference = new WeakReference<>(data);
+        data = null;
+        System.gc();
+        assertEquals(null, weakReference.get());
+    }
+}
