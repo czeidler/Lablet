f671b3e495ce996030b37c8727281a304f9edb31
Clemens <czei002@aucklanduni.ac.nz>
2014-12-02 07:55:28 +1300

    Add missing DataStatistics file.

---
+++ src/nz/ac/auckland/lablet/views/plotview/DataStatistics.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013-2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package nz.ac.auckland.lablet.views.plotview;
+
+import android.graphics.RectF;
+
+
+public abstract class DataStatistics {
+    private IListener listener;
+    protected RectF dataLimits = null;
+    protected RectF previousLimits = null;
+
+    public interface IListener {
+        public void onLimitsChanged(DataStatistics dataStatistics);
+    }
+
+    public void setListener(IListener listener) {
+        this.listener = listener;
+    }
+
+    protected void notifyLimitsChanged() {
+        if (listener != null)
+            listener.onLimitsChanged(this);
+    }
+
+
+    public RectF getDataLimits() {
+        if (dataLimits == null)
+            return null;
+        return new RectF(dataLimits);
+    }
+
+    public RectF getPreviousDataLimits() {
+        return previousLimits;
+    }
+
+    public abstract void release();
+
+    public abstract AbstractPlotDataAdapter getAdapter();
+}
