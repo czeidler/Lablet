536c9586246b8633fd2dbfc41531065835fa321b
Clemens <czei002@aucklanduni.ac.nz>
2015-03-25 15:39:23 +1300

    Add missing copyright.

--- app/src/main/java/nz/ac/auckland/lablet/camera/FrameRateSettingsDialog.java
+++ app/src/main/java/nz/ac/auckland/lablet/camera/FrameRateSettingsDialog.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2013-2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
 package nz.ac.auckland.lablet.camera;
 
 import android.app.AlertDialog;
