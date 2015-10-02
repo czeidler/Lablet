7fa983877aa0c534fe4a5a16f07f17d213e80706
Clemens Zeidler <clemens.zeidler@gmail.com>
2015-10-02 18:53:30 +1300

    Use an abstract base class.

--- app/src/main/java/nz/ac/auckland/lablet/experiment/MarkerDataModel.java
+++ app/src/main/java/nz/ac/auckland/lablet/experiment/MarkerDataModel.java
@@ -17,29 +17,21 @@ import java.util.Comparator;
 import java.util.List;
 
 
-/**
- * Data model for a list of marker.
- */
-public class MarkerDataModel extends WeakListenable<MarkerDataModel.IListener> {
+abstract class AbstractPointDataModel<T> extends WeakListenable<AbstractPointDataModel.IListener> {
     /**
      * Listener interface for the marker data model.
      */
-    public interface IListener {
-        void onDataAdded(MarkerDataModel model, int index);
-        void onDataRemoved(MarkerDataModel model, int index, MarkerData data);
-        void onDataChanged(MarkerDataModel model, int index, int number);
-        void onAllDataChanged(MarkerDataModel model);
-        void onDataSelected(MarkerDataModel model, int index);
+    public interface IListener<List extends AbstractPointDataModel, T> {
+        void onDataAdded(List model, int index);
+        void onDataRemoved(List model, int index, T data);
+        void onDataChanged(List model, int index, int number);
+        void onAllDataChanged(List model);
+        void onDataSelected(List model, int index);
     }
 
-    final protected List<MarkerData> markerDataList = new ArrayList<>();
     private int selectedDataIndex = -1;
 
-    public void setMarkerDataList(List<MarkerData> markerDataList) {
-        this.markerDataList.clear();
-        this.markerDataList.addAll(markerDataList);
-        notifyAllDataChanged();
-    }
+    abstract public int size();
 
     public void selectMarkerData(int index) {
         if (selectedDataIndex == index)
@@ -48,15 +40,122 @@ public class MarkerDataModel extends WeakListenable<MarkerDataModel.IListener> {
         notifyDataSelected(index);
     }
 
+    public int getSelectedMarkerData() {
+        return selectedDataIndex;
+    }
+
+    abstract public T getAt(int index);
+
+    abstract public PointF getPosition(int index);
+
+    abstract protected int addDataNoNotify(T data);
+    abstract protected T removeDataNoNotify(int index);
+    abstract protected void clearNoNotify();
+
+    public int addData(T data) {
+        int i = addDataNoNotify(data);
+        notifyDataAdded(i);
+        return i;
+    }
+
+    public T removeData(int index) {
+        T data = removeDataNoNotify(index);
+        notifyDataRemoved(index, data);
+        if (index == selectedDataIndex)
+            selectMarkerData(-1);
+        return data;
+    }
+
+    public void clear() {
+        clearNoNotify();
+        selectedDataIndex = -1;
+        notifyAllDataChanged();
+    }
+
+    public void notifyDataAdded(int index) {
+        for (IListener listener : getListeners())
+            listener.onDataAdded(this, index);
+    }
+
+    public void notifyDataRemoved(int index, T data) {
+        for (IListener listener : getListeners())
+            listener.onDataRemoved(this, index, data);
+    }
+
+    public void notifyDataChanged(int index, int number) {
+        for (IListener listener : getListeners())
+            listener.onDataChanged(this, index, number);
+    }
+
+    public void notifyAllDataChanged() {
+        for (IListener listener : getListeners())
+            listener.onAllDataChanged(this);
+    }
+
+    public void notifyDataSelected(int index) {
+        for (IListener listener : getListeners())
+            listener.onDataSelected(this, index);
+    }
+}
+
+abstract class AbstractPointDataList<T> extends AbstractPointDataModel<T> {
+    protected List<T> list = new ArrayList<>();
+
+    public void setMarkerDataList(List<T> dataList) {
+        this.list.clear();
+        this.list.addAll(dataList);
+        notifyAllDataChanged();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public T getAt(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    protected int addDataNoNotify(T data) {
+        int i = list.size();
+        list.add(data);
+        return i;
+    }
+
+    @Override
+    protected T removeDataNoNotify(int index) {
+        return list.remove(index);
+    }
+
+    @Override
+    protected void clearNoNotify() {
+        list.clear();
+    }
+}
+
+
+/**
+ * Data model for a list of marker.
+ */
+public class MarkerDataModel extends AbstractPointDataList<MarkerData> {
+    /**
+     * Listener interface for the marker data model.
+     */
+    public interface IListener extends AbstractPointDataModel.IListener<MarkerDataModel, MarkerData> {
+        void onDataAdded(MarkerDataModel model, int index);
+        void onDataRemoved(MarkerDataModel model, int index, MarkerData data);
+        void onDataChanged(MarkerDataModel model, int index, int number);
+        void onAllDataChanged(MarkerDataModel model);
+        void onDataSelected(MarkerDataModel model, int index);
+    }
+
     public void selectMarkerData(MarkerData markerData) {
-        int index = markerDataList.indexOf(markerData);
+        int index = list.indexOf(markerData);
         if (index < 0)
             return;
         selectMarkerData(index);
-    }
-
-    public int getSelectedMarkerData() {
-        return selectedDataIndex;
     }
 
     public void setMarkerPosition(PointF position, int index) {
@@ -72,24 +171,24 @@ public class MarkerDataModel extends WeakListenable<MarkerDataModel.IListener> {
     public int addMarkerData(MarkerData data, boolean sort) {
         int i = 0;
         if (sort) {
-            for (; i < markerDataList.size(); i++) {
-                MarkerData current = markerDataList.get(i);
+            for (; i < list.size(); i++) {
+                MarkerData current = list.get(i);
                 if (current.getId() == data.getId())
                     return -1;
                 if (current.getId() > data.getId())
                     break;
             }
         } else
-            i = markerDataList.size();
+            i = list.size();
 
-        markerDataList.add(i, data);
+        list.add(i, data);
         notifyDataAdded(i);
         return i;
     }
 
     public int getLargestRunId() {
         int runId = -1;
-        for (MarkerData markerData : markerDataList) {
+        for (MarkerData markerData : list) {
             if (markerData.getId() > runId)
                 runId = markerData.getId();
         }
@@ -97,11 +196,11 @@ public class MarkerDataModel extends WeakListenable<MarkerDataModel.IListener> {
     }
 
     public int getMarkerCount() {
-        return markerDataList.size();
+        return size();
     }
 
     public MarkerData getMarkerDataAt(int index) {
-        return markerDataList.get(index);
+        return getAt(index);
     }
 
     public Bundle toBundle() {
@@ -146,27 +245,22 @@ public class MarkerDataModel extends WeakListenable<MarkerDataModel.IListener> {
         return -1;
     }
 
+    @Override
+    public PointF getPosition(int index) {
+        return getRealMarkerPositionAt(index);
+    }
+
     public PointF getRealMarkerPositionAt(int index) {
         MarkerData data = getMarkerDataAt(index);
         return data.getPosition();
     }
 
     public MarkerData removeMarkerData(int index) {
-        MarkerData data = markerDataList.remove(index);
-        notifyDataRemoved(index, data);
-        if (index == selectedDataIndex)
-            selectMarkerData(-1);
-        return data;
-    }
-
-    public void clear() {
-        markerDataList.clear();
-        selectedDataIndex = -1;
-        notifyAllDataChanged();
+        return removeData(index);
     }
 
     public void sort(Comparator<? super MarkerData> comparator) {
-        Collections.sort(markerDataList, comparator);
+        Collections.sort(list, comparator);
         notifyAllDataChanged();
     }
 
@@ -187,30 +281,4 @@ public class MarkerDataModel extends WeakListenable<MarkerDataModel.IListener> {
             }
         });
     }
-
-    public void notifyDataAdded(int index) {
-        for (IListener listener : getListeners())
-            listener.onDataAdded(this, index);
-    }
-
-    public void notifyDataRemoved(int index, MarkerData data) {
-        for (IListener listener : getListeners())
-            listener.onDataRemoved(this, index, data);
-    }
-
-    public void notifyDataChanged(int index, int number) {
-        for (IListener listener : getListeners())
-            listener.onDataChanged(this, index, number);
-    }
-
-    public void notifyAllDataChanged() {
-        for (IListener listener : getListeners())
-            listener.onAllDataChanged(this);
-    }
-
-    public void notifyDataSelected(int index) {
-        for (IListener listener : getListeners())
-            listener.onDataSelected(this, index);
-    }
-
 }
